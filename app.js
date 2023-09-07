a6f02cc54d639bb7c5a140c17b48bd888d0ebd47
Anwara Favour <favourchika02@gmail.com>
2023-09-07 15:16:39 +0100

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any port you prefer
+const port = 3000; 
 
 app.get('/api', (req, res) => {
   const slack_name = req.query.slack_name;
